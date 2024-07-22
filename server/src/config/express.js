5b7e9016fdfd663e2913d6cd3cff939475ac9855
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2024-07-22 12:37:09 +0300

    added cors to the server

--- server/src/config/express.js
+++ server/src/config/express.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 
 const { session } = require('../middlewares/session');
+const { cors } = require('../middlewares/cors');
 
 const secret = 'Super secr3t';
 
 function configExpress(app) {
 
     app.use(cookieParser({ secret }));
+    app.use(cors());
     app.use(session());
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
