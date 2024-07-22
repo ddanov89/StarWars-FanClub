692fb6bedf70d24b387faf5d79df3f5e366b8af9
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2024-07-22 14:55:09 +0300

    Added Catalog page

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,20 +1,34 @@
+import { useEffect, useState } from "react";
+import * as moviesAPI from "../../api/movies-api";
+import MovieItem from "./movieItem/MovieItem";
+
 export default function Catalog() {
- return (
- <>
- <main id="catalog">
-    <section className="movie-market">
-        <h1>Universe Catalog</h1>
-        
-        <div className="movie-list">
-            {/* movies */}
-        </div>
-         {/* no movies */}
-        <div className="no-movie">
-            <p>There are no movie/games added yet!</p>
-        </div>
-        
-    </section>
-</main>
- </>
- );
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const result = await moviesAPI.getAll();
+      setMovies(result);
+    })();
+  }, []);
+
+  return (
+    <>
+      <main id="catalog">
+        <section className="movie-market">
+          <h1>Universe Catalog</h1>
+
+          <div className="movie-list">
+            {movies.length > 0 ? (
+              movies.map((movie) => <MovieItem key={movie._id} {...movie} />)
+            ) : (
+              <div className="no-movie">
+                <p>There are no movie/games added yet!</p>
+              </div>
+            )}
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
