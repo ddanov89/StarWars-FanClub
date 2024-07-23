cfc5094f803007ead18edc5e47d2468014e4d024
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2024-07-23 07:34:56 +0300

    Details page added and as well logic to visualize content

--- client/src/components/movieDetails/MovieDetails.jsx
+++ client/src/components/movieDetails/MovieDetails.jsx
@@ -1,43 +1,63 @@
-import { Link } from "react-router-dom"
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import * as moviesAPI from "../../api/movies-api";
 
 export default function MovieDetails() {
- return (
- <>
- <main id="details">
+  const [movie, setMovie] = useState({});
+  const { id } = useParams();
 
-    <section id="details-info">
-        <div className="movie-image">
-            <img src={image}/>
-        </div>
-        <div className="movie-info">
+  useEffect(() => {
+    (async () => {
+      const result = await moviesAPI.getOne(id);
+      setMovie(result);
+    })();
+  }, []);
+  return (
+    <>
+      <main id="details">
+        <section id="details-info">
+          <div className="movie-image">
+            <img src={movie.image} />
+          </div>
+          <div className="movie-info">
             <div className="movie-text">
-                <h1 id="name">{}</h1>
-                <h3 id="category">Category: {category}</h3>
-                <p id="rating"><span>Rating: {rating}</span></p>
-                <p id="review">Review: <br/>{review}</p>
-                <p id="description">Description: <br/>{description}</p>
+              <h1 id="name">{movie.name}</h1>
+              <h3 id="category">Category: {movie.category}</h3>
+              <p id="rating">
+                <span>Rating: {movie.rating}</span>
+              </p>
+              <p id="review">
+                Review: <br />
+                {movie.review}
+              </p>
+              <p id="description">
+                Description: <br />
+                {movie.description}
+              </p>
             </div>
 
             {/* if user */}
 
             <div className="product-btn">
+              {/* if author */}
+              <div className="author">
+                <Link to={`/edit/${movie._id}`} className="btn-edit">
+                  Edit
+                </Link>
+                <Link to={`/delete/${movie._id}`} className="btn-delete">
+                  Delete
+                </Link>
+              </div>
 
-                {/* if author */}
-                <div className="author">
-                    <Link to={`/edit/${id}`} className="btn-edit">Edit</Link>
-                    <Link to={`/delete/${id}`} className="btn-delete">Delete</Link>
-                </div>
-                
-                {/* if hasBeenLiked */}
-                <p className="like-message">You have already liked this movie!</p>
-                <Link to={`/like/${id}`} className="btn-like">Like</Link>
-                
+              {/* if hasBeenLiked */}
+              <p className="like-message">You have already liked this movie!</p>
+              <Link to={`/like/${movie._id}`} className="btn-like">
+                Like
+              </Link>
             </div>
-            
-        </div>
-        
-    </section>
-</main>
- </>
- );
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
