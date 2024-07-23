bcfab56fe5bee60371c8b6a1fc9f0991762fbde3
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2024-07-23 07:32:01 +0300

    Search component added

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -2,11 +2,11 @@ export default function Search() {
  return (
  <>
  <main id="search">
-    <section class="movie-search">
+    <section className="movie-search">
         <h1>Search the Universe</h1>
 
-        <form class="search-form" name="search-form" method="get">
-            <input type="text" class="search-movie" name="name" placeholder="Search here..." value=""/>
+        <form className="search-form" name="search-form" method="get">
+            <input type="text" className="search-movie" name="name" placeholder="Search here..." value=""/>
             <select autocomplete="off" id="category" name="category" value="" selected="">
                 <option value="" hidden="hidden" selected>""</option>
                 <option value="---">---</option>
@@ -14,17 +14,17 @@ export default function Search() {
                 <option value="Series">Series</option>
                 <option value="Game">Game</option>
             </select>
-            <button type="submit" class="btn-search">Search</button>
+            <button type="submit" className="btn-search">Search</button>
         </form>
 
         
-        <div class="search-result">
-            <div class="search-list">
+        <div className="search-result">
+            <div className="search-list">
                 {/* show search results */}
             </div>
         </div>
 
-        <div class="no-match">
+        <div className="no-match">
             <p>There are no movies/games found!</p>
         </div>
     </section>
