497416bfa23a86178e9fe379eeeb642a343305e0
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2024-07-23 07:31:25 +0300

    MovieItem added

--- client/src/components/catalog/movieItem/MovieItem.jsx
+++ client/src/components/catalog/movieItem/MovieItem.jsx
@@ -1,22 +1,28 @@
 import { Link } from "react-router-dom";
 
-export default function MovieItem() {
+export default function MovieItem({ 
+  image, 
+  name, 
+  category, 
+  rating, 
+  _id
+}) {
   return (
     <>
-      <div class="movie">
-        <div class="movie-img">
-          <img src="image" />
+      <div className="movie">
+        <div className="movie-img">
+          <img src={image} />
         </div>
-        <div class="movie-info">
-          <h1>"name"</h1>
+        <div className="movie-info">
+          <h1>{name}</h1>
           <p>
-            <span>Category: </span>"category"
+            <span>Category: </span>{category}
           </p>
           <p>
-            <span>Rating: </span>"rating"
+            <span>Rating: </span>{rating}
           </p>
         </div>
-        <Link to={`/catalog/${_id}`} class="btn-details">
+        <Link to={`/catalog/${_id}`} className="btn-details">
           Details
         </Link>
       </div>
