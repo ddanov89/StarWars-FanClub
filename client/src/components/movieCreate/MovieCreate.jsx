84e05828b235139c638f7817661e73e08b32c318
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2024-07-24 07:14:02 +0300

    implemented logic for media creation

--- client/src/components/movieCreate/MovieCreate.jsx
+++ client/src/components/movieCreate/MovieCreate.jsx
@@ -1,34 +1,64 @@
+import { useNavigate } from "react-router-dom";
+import moviesAPI from "../../api/movies-api";
+
 export default function MovieCreate() {
- return (
- <>
- <main id="create">
-    <section id="create-container">
-        <div className="create-container-info">
+  const navigate = useNavigate();
+
+  const createSubmitHandler = async (e) => {
+    e.preventDefault();
+    const movieData = Object.fromEntries(new FormData(e.currentTarget));
+    try {
+      await moviesAPI.create(movieData);
+      navigate("/");
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <>
+      <main id="create">
+        <section id="create-container">
+          <div className="create-container-info">
             <h1>Add to the Universe</h1>
-            <form method="post" name="create-form">
-                <label>Category:</label>
-                <select autocomplete="off" id="category" name="category" value="" selected="" >
-                    <option value="" hidden="hidden" selected>""</option>
-                    <option value="---" hidden="hidden" selected>---</option>
-                    <option value="Movie">Movie</option>
-                    <option value="Series">Series</option>
-                    <option value="Game">Game</option>
-                </select>
-                <label>Name:</label>
-                <input type="text" id="name" name="name" value="name"/>
-                <label>Image:</label>
-                <input type="text" id="image" name="image" placeholder="http://..." value="image"/>
-                <label>Rating:</label>
-                <input type="number" id="rating" name="rating"value="rating"/>
-                <label>Review:</label>
-                <textarea id="review" name="review">review</textarea>
-                <label>Description:</label>
-                <textarea id="description" name="description">description</textarea>
-                <input type="submit" id="btn" value="Add"></input>
+            <form name="create-form" onSubmit={createSubmitHandler}>
+              <label>Category:</label>
+              <select
+                autocomplete="off"
+                id="category"
+                name="category"
+                selected=""
+              >
+                <option value="" hidden="hidden" selected>
+                  ""
+                </option>
+                <option value="---" hidden="hidden" selected>
+                  ---
+                </option>
+                <option value="Movie">Movie</option>
+                <option value="Series">Series</option>
+                <option value="Game">Game</option>
+              </select>
+              <label>Name:</label>
+              <input type="text" id="name" name="name" />
+              <label>Image:</label>
+              <input
+                type="text"
+                id="image"
+                name="image"
+                placeholder="http://..."
+              />
+              <label>Rating:</label>
+              <input type="number" id="rating" name="rating" />
+              <label>Review:</label>
+              <textarea id="review" name="review"></textarea>
+              <label>Description:</label>
+              <textarea id="description" name="description"></textarea>
+              <input type="submit" id="btn" value="Add"></input>
             </form>
-        </div>
-    </section>
-</main>
- </>
- );
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
