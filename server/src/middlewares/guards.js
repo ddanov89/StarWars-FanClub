684a4a8e83677bcb68ac53a1610d9777c09eb523
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2024-07-26 17:17:03 +0300

    added guards to server

---
+++ server/src/middlewares/guards.js
@@ -0,0 +1,21 @@
+function isUser() {
+    return function (req, res, next) {
+        if (!req.user) {
+            res.status(401).json({code:401, message: 'Missing credentials. Please login!'});
+        } else {
+            next();
+        }
+    };
+}
+
+function isGuest() {
+    return function (req, res, next) {
+        if (req.user) {
+            res.redirect('/');
+        } else {
+            next();
+        }
+    };
+}
+
+module.exports = { isUser, isGuest };
