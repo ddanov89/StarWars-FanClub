b4fa70d30ba9a12f03c009de56712e21886ff17e
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2024-07-28 21:50:29 +0300

    persisted auth state implemented

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -1,38 +1,40 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext } from "react";
+import usePersistedState from "../hooks/usePersistedState";
 
 export const AuthContext = createContext({
-    _id: "",
-    email: "",
-    token: "",
-    isAuthenticated: false,
-    changeAuthState: (authState = {}) => null,
+  _id: "",
+  email: "",
+  token: "",
+  isAuthenticated: false,
+  changeAuthState: (authState = {}) => null,
 });
 
 export function AuthContextProvider(props) {
-    
-    const [authState, setAuthState] = useState({});
+  const [authState, setAuthState] = usePersistedState("auth", () => ({}));
 
-    const changeAuthState = (state) => {
-        //TODO: Quick solution, fix by implementing persisting authState
-        localStorage.setItem('token', state.token);
+  const changeAuthState = (state) => {
+    //TODO: Quick solution, fix by implementing persisting authState
+    localStorage.setItem("token", state.token);
 
-        setAuthState(state);
-    };
+    setAuthState(state);
+  };
 
-    const contextData = {
-        _id: authState._id,
-        email: authState.email,
-        token: authState.token,
-        isAuthenticated: !!authState.email,
-        changeAuthState,
-      };
+  const contextData = {
+    _id: authState._id,
+    email: authState.email,
+    token: authState.token,
+    isAuthenticated: !!authState.email,
+    changeAuthState,
+  };
 
-    return (<AuthContext.Provider value={contextData}>
-        {props.children}
-    </AuthContext.Provider>);
+  return (
+    <AuthContext.Provider value={contextData}>
+      {props.children}
+    </AuthContext.Provider>
+  );
 }
 
 export function useAuthContext() {
-    const authData = useContext(AuthContext);
-    return authData;
+  const authData = useContext(AuthContext);
+  return authData;
 }
