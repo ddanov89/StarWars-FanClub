8de592aa817f33320185ffc5370ac48a0f044211
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2024-08-01 12:38:52 +0300

    implemented a like functionality

---
+++ client/src/components/like/Like.jsx
@@ -0,0 +1,14 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { usePostLike } from "../../hooks/useLike";
+
+export default function Like() {
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  try {
+    const [like, setLike] = usePostLike();
+    navigate(`/catalog/${id}`);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
