d96a97530c11376aadde3570ef55f3ea4f0dcff7
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2024-08-01 12:38:24 +0300

    added hook for liking a movie

---
+++ client/src/hooks/useLike.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import moviesAPI from "../api/movies-api";
+import { useParams } from "react-router-dom";
+import { useAuthContext } from "../contexts/AuthContext";
+
+export function useGetIsLiked(movieId, userId) {
+    const [isLiked, setIsLiked] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            const movie = await moviesAPI.getOne(movieId);
+            const movieLikes = await movie.likedBy;
+            const result = movieLikes.find(like => like.toString() == userId);
+            setIsLiked(!!result);
+        })();
+    }, []);
+    return [isLiked, setIsLiked];
+}
+
+export function usePostLike() {
+    const { id } = useParams();
+    const { _id } = useAuthContext();
+    const userId = _id;
+
+  const [like, setLike] = useState({});
+
+  useEffect(() => {
+      (async () => {
+        const userData = {
+            userId: userId
+        };
+        const result = await moviesAPI.like(id, userData);
+          setLike(result);
+      })();
+  }, []);
+  return [like, setLike];
+}
